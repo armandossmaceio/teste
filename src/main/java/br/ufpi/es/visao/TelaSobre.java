77cb57e609c81d812db4fdaaaf17a60d7fb25c77
armandossrecife <armando.sousa@gmail.com>
2017-09-11 12:05:24 -0300

    Novo colaborador na tela sobre.

--- src/main/java/br/ufpi/es/visao/TelaSobre.java
+++ src/main/java/br/ufpi/es/visao/TelaSobre.java
@@ -4,5 +4,6 @@ package br.ufpi.es.visao;
 public class TelaSobre {
 	public void mensagem(){
 		System.out.println("Bem vindo");
+		System.out.println("Linha adicionada por outro developer local no eclipse");
 	}
 }
