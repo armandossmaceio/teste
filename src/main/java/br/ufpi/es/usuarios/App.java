8f845a87ab712ba0c854b209a8c3213e7a5e470c
armandossrecife <armando.sousa@gmail.com>
2017-09-11 11:43:53 -0300

    Adicionado o controle de logs na aplicação.

--- src/main/java/br/ufpi/es/usuarios/App.java
+++ src/main/java/br/ufpi/es/usuarios/App.java
@@ -1,13 +1,23 @@
 package br.ufpi.es.usuarios;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    static Logger logger = LogManager.getLogger();
     public static void main( String[] args )
+
     {
         System.out.println( "Hello World!" );
+        
+
+		logger.debug("Registro de debug");
+		logger.info("Registro de informação");
+		logger.error("Registro de erro");
     }
 }
--- src/main/java/br/ufpi/es/visao/TelaPrincipal.java
+++ src/main/java/br/ufpi/es/visao/TelaPrincipal.java
@@ -2,6 +2,9 @@ package br.ufpi.es.visao;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import br.ufpi.es.controle.ControladorUsuario;
 import br.ufpi.es.dados.IRepositorioUsuario;
 import br.ufpi.es.dados.RepositorioLista;
@@ -9,6 +12,7 @@ import br.ufpi.es.dados.RepositorioVetorUsuarios;
 import br.ufpi.es.dados.Usuario;
 
 public class TelaPrincipal {
+	static Logger logger = LogManager.getLogger();
 
 	public static void main(String[] args) {
 		//cria usuarios
@@ -37,6 +41,9 @@ public class TelaPrincipal {
 			System.out.println("Id: " + u.getId() + " - " + u.getNome());
 		}
 
+		logger.debug("Registro de debug");
+		logger.info("Registro de informação");
+		logger.error("Registro de erro");
 	}
 
 }
