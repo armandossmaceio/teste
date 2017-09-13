a2d63e82c4616b698407f04d81df4e795792dd8e
armandossrecife <armando.sousa@gmail.com>
2017-09-13 16:10:08 -0300

    Alterado nomes dos parâmetros do repositório Lista.

--- src/main/java/br/ufpi/es/dados/RepositorioLista.java
+++ src/main/java/br/ufpi/es/dados/RepositorioLista.java
@@ -19,7 +19,7 @@ public class RepositorioLista implements IRepositorioUsuario{
 		return lista.get(index);
 	}
 
-	public void alterar(Usuario original, Usuario novo) {
+	public void alterar(Usuario usuarioOriginal, Usuario novoUsuario) {
 		// TODO Auto-generated method stub
 	}
 
