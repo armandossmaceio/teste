368604914b1a94eef46ac731088f7adcc5ae04e7
armandossrecife <armando.sousa@gmail.com>
2017-09-13 15:56:52 -0300

    Alterado parâmetro de passagem na Interface IRepositorioUsuario

--- src/main/java/br/ufpi/es/dados/IRepositorioUsuario.java
+++ src/main/java/br/ufpi/es/dados/IRepositorioUsuario.java
@@ -3,8 +3,8 @@ package br.ufpi.es.dados;
 import java.util.List;
 
 public interface IRepositorioUsuario {
-	public void inserir(Usuario u);
+	public void inserir(Usuario valor);
 	public List<Usuario> listar();
-	public Usuario buscar(Usuario u);
+	public Usuario buscar(Usuario valor);
 	public void alterar(Usuario original, Usuario novo);
 }
