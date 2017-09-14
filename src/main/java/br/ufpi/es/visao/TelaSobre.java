040ef980f38744e404a037690d7fd3b5405bb014
armandossrecife <armando.sousa@gmail.com>
2017-09-14 13:17:34 -0300

    Adicionados os getters and setters na Tela Sobre.

--- src/main/java/br/ufpi/es/visao/TelaSobre.java
+++ src/main/java/br/ufpi/es/visao/TelaSobre.java
@@ -1,7 +1,25 @@
 package br.ufpi.es.visao;
 
 public class TelaSobre {
-	String titulo;
-	String link;
-	String colaboradores;
+	private String titulo;
+	private String link;
+	private String colaboradores;
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getLink() {
+		return link;
+	}
+	public void setLink(String link) {
+		this.link = link;
+	}
+	public String getColaboradores() {
+		return colaboradores;
+	}
+	public void setColaboradores(String colaboradores) {
+		this.colaboradores = colaboradores;
+	}
 }
